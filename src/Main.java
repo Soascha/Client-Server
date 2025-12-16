df8e998b2af410c61ac8226c6baf860707cadebf
kosychsa <kosych.sascha@gmail.com>
2025-12-16 13:24:55 +0100

    neu

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args){
         Connection c = new Connection("time-b.timefreq.bldrdoc.gov", 13);
 
+        String Leerzeile = c.receive();
         String serverTime = c.receive();
         System.out.println("Serverzeit: " + serverTime);
 
